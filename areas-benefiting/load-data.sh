c1a77553832aa000f05b3b2661cb148d5e1cf94c
Tedd Mason <tjmason.ea@gmail.com>
2016-08-09 16:49:29 +0100

    reverting path

--- areas-benefiting/load-data.sh
+++ areas-benefiting/load-data.sh
@@ -10,7 +10,7 @@ ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_D
 
 echo Areas Benefiting running database scripts
 
-psql "user=${FMP_DB_USER} password=${FMP_DB_PASSWORD} host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME}" -f ./areas-benefiting/run.sql -X --echo-queries --set ON_ERROR_STOP=on -w
+psql "user=${FMP_DB_USER} password=${FMP_DB_PASSWORD} host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME}" -f run.sql -X --echo-queries --set ON_ERROR_STOP=on -w
 
 echo Finished loading Areas Benefiting
 
