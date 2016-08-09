f21d7c5fba4f19b9b4d3e5a0d1b522f5cf67af1c
Tedd Mason <tjmason.ea@gmail.com>
2016-08-09 15:54:55 +0100

    db naming

--- setup.sql
+++ setup.sql
@@ -5,5 +5,5 @@ GRANT ALL ON SCHEMA postgis TO fmp;
 -- Create fmp schema
 CREATE SCHEMA fmp AUTHORIZATION fmp;
 
-ALTER DATABASE fmp-development SET search_path = "$user", public, postgis, topology;
+ALTER DATABASE "fmp-development" SET search_path = "$user", public, postgis, topology;
 ALTER EXTENSION postgis set schema postgis;
