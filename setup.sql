4614ba51d9ad07a3da63ec9894f7163fa8fd3267
Tedd Mason <tjmason.ea@gmail.com>
2016-08-09 15:58:03 +0100

    stripping db setup

--- setup.sql
+++ setup.sql
@@ -1,9 +1,2 @@
--- CREATE postgis schema
-CREATE SCHEMA postgis AUTHORIZATION fmp;
-GRANT ALL ON SCHEMA postgis TO fmp;
-
 -- Create fmp schema
 CREATE SCHEMA fmp AUTHORIZATION fmp;
-
-ALTER DATABASE "fmp-development" SET search_path = "$user", public, postgis, topology;
-ALTER EXTENSION postgis set schema postgis;
