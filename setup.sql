ad5e3784722155a6ead3f04ddcc84f4692a1a8c7
Tedd Mason <tjmason.ea@gmail.com>
2016-08-10 08:57:01 +0100

    tablespaces

--- setup.sql
+++ setup.sql
@@ -1,2 +1,6 @@
+-- Create tablespaces
+CREATE TABLESPACE fmp_tables OWNER fmp LOCATION '/fmp_tables';
+CREATE TABLESPACE fmp_indexes OWNER fmp LOCATION '/fmp_indexes';
+
 -- Create fmp schema
 CREATE SCHEMA fmp AUTHORIZATION fmp;
