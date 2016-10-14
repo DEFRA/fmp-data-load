9932066709ccb8737f69fe4f9e7049b0fe1bec35
teddmason <teddmason@gmail.com>
2016-10-14 13:09:07 +0100

    New fmp dataset versions

--- defences/load-data.sh
+++ defences/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading Defences into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_defences_v201602${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.defences -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_defences_v201602
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_defences_v201611${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.defences -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_defences_v201611
 
 echo Finished loading Defences
 
--- floodzone-3/load-data.sh
+++ floodzone-3/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading Flood Zone 3 into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_floodzone3_v201602_WIYBY${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.floodzone_3 -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_floodzone3_v201602_WIYBY
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_floodzone3_v201611_WIYBY${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.floodzone_3 -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_floodzone3_v201611_WIYBY
 
 echo Finished loading Flood Zone 3
 
