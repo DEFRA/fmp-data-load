7d5e503fab2476295b38ee5821a471462b539791
Paul Shaw <pmshaw.ea@gmail.com>
2017-07-14 13:54:50 +0100

    August 2017 data load update

--- flood-storage-areas/load-data.sh
+++ flood-storage-areas/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading Flood Storage Areas into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_fsa_v201708${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.flood_storage_areas -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_fsa_v201705
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_fsa_v201708${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.flood_storage_areas -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_fsa_v201708
 
 echo Finished loading Flood Storage Areas
 
