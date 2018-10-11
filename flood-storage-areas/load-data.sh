d2303c1fe14412ff05d84f1a55fe519374639355
Tedd Mason <teddmason@gmail.com>
2018-10-11 14:21:19 +0100

    FLO-3142, move from shapefile to GDB

--- flood-storage-areas/load-data.sh
+++ flood-storage-areas/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading Flood Storage Areas into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_fsa_${FMP_FILE_VERSION}${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.flood_storage_areas -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_fsa_${FMP_FILE_VERSION}
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}FMfP_portal_${FMP_FILE_VERSION}.gdb -nln ${FMP_DB_SCHEMA}.flood_storage_areas nat_fsa_${FMP_FILE_VERSION} -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO nat_fsa_${FMP_FILE_VERSION}
 
 echo Finished loading Flood Storage Areas
 
--- floodzone-3/load-data.sh
+++ floodzone-3/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading Flood Zone 3 into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}nat_floodzone3_${FMP_FILE_VERSION}_5k_portal${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.floodzone_3 -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO -sql "SELECT fid_nat_fl,layer,type,fid_os_5km,name FROM nat_floodzone3_${FMP_FILE_VERSION}_5k_portal"
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}FMfP_portal_${FMP_FILE_VERSION}.gdb -nln ${FMP_DB_SCHEMA}.floodzone_3 nat_floodzone3_${FMP_FILE_VERSION}_5k_portal -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO -sql "SELECT fid_nat_fl,layer,type,fid_os_5km,name FROM nat_floodzone3_${FMP_FILE_VERSION}_5k_portal"
 
 echo Finished loading Flood Zone 3
 
