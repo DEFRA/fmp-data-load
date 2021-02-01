fdb697135f5cce30c23932e0a67ce43c30bc8609
paul shaw <paul.m.shaw@environment-agency.gov.uk>
2021-02-01 14:43:40 +0000

    GDAL 3.2.1 upgrade

--- main-rivers/load-data.sh
+++ main-rivers/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading Main Rivers into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}main_rivers_10k${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.main_rivers_10k -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO main_rivers_10k
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}main_rivers_10k${FMP_GDB_POSTFIX}.shp -nln ${FMP_DB_SCHEMA}.main_rivers_10k -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX NO -sql "SELECT status,length_km FROM main_rivers_10k" -lco GEOMETRY_NAME=wkb_geometry
 
 echo Finished loading Main Rivers
 
