b79c507dfad4597cd5315a604e13c3fa0261ce9a
Paul Shaw <paul.m.shaw@environment-agency.gov.uk>
2021-02-02 11:41:03 +0000

    GDAL 3.2.1 upgrade (#1)

--- england-10k/load-data.sh
+++ england-10k/load-data.sh
@@ -2,7 +2,7 @@
 
 echo Loading england-10k into database
 
-ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}england_010k.shp -nln ${FMP_DB_SCHEMA}.england -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX YES
+ogr2ogr -a_srs "EPSG:27700" -f "PostgreSQL" PG:"host=${FMP_DB_HOST} port=${FMP_DB_PORT} dbname=${FMP_DB_NAME} user=${FMP_DB_USER} password=${FMP_DB_PASSWORD}" ${FMP_GDB_ROOT}england_010k.shp -nln ${FMP_DB_SCHEMA}.england -overwrite -progress --config PG_USE_COPY YES -nlt PROMOTE_TO_MULTI -lco SPATIAL_INDEX=${FMP_GENERATE_SPATIAL_INDEX} --config SPATIAL_INDEX YES -lco GEOMETRY_NAME=wkb_geometry
 
 echo Finished loading england-10k
 
